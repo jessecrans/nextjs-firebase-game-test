cacfcf253e007880caed381a70de4ecca62ce9bb
JesseCrans <jesse.crans@live.nl>
2024-02-14 16:37:05 +0100

    attempt 8

--- src/app/api/lobby-cleanup/route.ts
+++ src/app/api/lobby-cleanup/route.ts
@@ -4,21 +4,23 @@ import { Game } from "@/lib/firebase/database";
 
 export const runtime = 'edge';
 
-export const GET = async (request: Request) => {
+export const GET = (request: Request) => {
   const cutOffTime = Date.now() - 1000 * 60 * 5; // 5 minutes ago
   const gamesRef = ref(db, 'games');
   console.log("GET /api/lobby-cleanup")
-  await get(gamesRef).then(async (snapshot) => {
+
+  get(gamesRef).then((snapshot) => {
     console.log("Data read successfully", snapshot.val());
     for (const [gameId, game] of Object.entries<Game>(snapshot.val())) {
       if (game.lastMoveTime < cutOffTime) {
         console.log(`Game ${gameId} is too old and will be removed.`);
-        await remove(ref(db, `games/${gameId}`));
+        remove(ref(db, `games/${gameId}`));
       }
     }
+    return new Response("Lobbies cleaned up.");
   }).catch((error) => {
     console.error("Error getting data:", error);
+    return new Response("Lobbies cleaned up.");
   });
 
-  return new Response("Lobbies cleaned up.");
 }
