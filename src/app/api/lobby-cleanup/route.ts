622617cf144c0550d8739c73bc4272ca082e10ec
JesseCrans <jesse.crans@live.nl>
2024-02-14 22:18:21 +0100

    debug logs

--- src/app/api/lobby-cleanup/route.ts
+++ src/app/api/lobby-cleanup/route.ts
@@ -2,19 +2,25 @@ import { ref, remove, get } from "firebase/database";
 import { db } from "@/lib/firebase/firebase";
 import { Game } from "@/lib/firebase/database";
 
-export const runtime = 'nodejs';
+export const runtime = 'edge';
 
 export const GET = async (request: Request) => {
-  console.log("Cleaning up lobbies...")
+  const getID = Math.round(Math.random() * 100)
+
+  console.log("Cleaning up lobbies...", getID);
 
   const cutOffTime = Date.now() - 1000 * 60 * 5; // 5 minutes ago
   const gamesRef = ref(db, 'games');
+
+  console.log("Getting games...", getID);
+
   const snapshot = await get(gamesRef);
-  console.log("Data read successfully", snapshot.val());
+
+  console.log("Data read successfully", getID, snapshot.val());
 
   for (const [gameId, game] of Object.entries<Game>(snapshot.val())) {
     if (game.lastMoveTime < cutOffTime) {
-      console.log(`Game ${gameId} is too old and will be removed.`);
+      console.log(`Game ${gameId} is too old and will be removed.`, getID);
       remove(ref(db, `games/${gameId}`));
     }
   }
