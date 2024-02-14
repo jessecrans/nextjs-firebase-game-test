fba42e999bf92bd01da7e2d74becd658ed899dd2
JesseCrans <jesse.crans@live.nl>
2024-02-14 15:53:34 +0100

    attempt to fix bug where lobby cleanup is only working in dev mode

--- src/app/api/lobby-cleanup/route.ts
+++ src/app/api/lobby-cleanup/route.ts
@@ -7,13 +7,16 @@ export const runtime = 'edge';
 export const GET = (request: Request) => {
   const cutOffTime = Date.now() - 1000 * 60 * 5; // 5 minutes ago
   const gamesRef = ref(db, 'games');
-  get(gamesRef).then((snapshot) => {
+  get(gamesRef).then(async (snapshot) => {
     for (const [gameId, game] of Object.entries<Game>(snapshot.val())) {
       if (game.lastMoveTime < cutOffTime) {
-        remove(ref(db, `games/${gameId}`));
+        console.log(`Game ${gameId} is too old and will be removed.`);
+        await remove(ref(db, `games/${gameId}`));
       }
     }
-  })
+  }).catch((error) => {
+    console.error("Error getting data:", error);
+  });
 
   return new Response("Lobbies cleaned up.");
 }
