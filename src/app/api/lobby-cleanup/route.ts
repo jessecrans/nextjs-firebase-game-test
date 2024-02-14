547dcbc539e6f0238a54875d29924b7241f7b48f
JesseCrans <jesse.crans@live.nl>
2024-02-14 21:44:22 +0100

    attempt to fix lobby cleanup by changing then for await
    Co-authored-by: Hugo van Schalm <HugovanSchalm@users.noreply.github.com>

--- src/app/api/lobby-cleanup/route.ts
+++ src/app/api/lobby-cleanup/route.ts
@@ -5,21 +5,19 @@ import { Game } from "@/lib/firebase/database";
 export const runtime = 'edge';
 
 export const GET = async (request: Request) => {
+  console.log("Cleaning up lobbies...")
+
   const cutOffTime = Date.now() - 1000 * 60 * 5; // 5 minutes ago
   const gamesRef = ref(db, 'games');
-  console.log("GET /api/lobby-cleanup")
+  const snapshot = await get(gamesRef)
+  console.log("Data read successfully", snapshot.val());
 
-  await get(gamesRef).then((snapshot) => {
-    console.log("Data read successfully", snapshot.val());
-    for (const [gameId, game] of Object.entries<Game>(snapshot.val())) {
-      if (game.lastMoveTime < cutOffTime) {
-        console.log(`Game ${gameId} is too old and will be removed.`);
-        remove(ref(db, `games/${gameId}`));
-      }
+  for (const [gameId, game] of Object.entries<Game>(snapshot.val())) {
+    if (game.lastMoveTime < cutOffTime) {
+      console.log(`Game ${gameId} is too old and will be removed.`);
+      remove(ref(db, `games/${gameId}`));
     }
-  }).catch((error) => {
-    console.error("Error getting data:", error);
-  });
+  }
 
   return new Response("Lobbies cleaned up.");
 }
