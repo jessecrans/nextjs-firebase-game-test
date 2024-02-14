8f3fbb900575f33ddaa73cc15b3daf3b8657fb38
JesseCrans <jesse.crans@live.nl>
2024-02-14 16:08:52 +0100

    attempt 4

--- src/app/api/lobby-cleanup/route.ts
+++ src/app/api/lobby-cleanup/route.ts
@@ -2,7 +2,7 @@ import { ref, remove, get } from "firebase/database";
 import { db } from "@/lib/firebase/firebase";
 import { Game } from "@/lib/firebase/database";
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 export const GET = async (request: Request) => {
   const cutOffTime = Date.now() - 1000 * 60 * 5; // 5 minutes ago
