b2c5c3442be70c68817bce8b0a70f3cee8543193
JesseCrans <jesse.crans@live.nl>
2024-02-14 21:47:07 +0100

    trying with nodejs

--- src/app/api/lobby-cleanup/route.ts
+++ src/app/api/lobby-cleanup/route.ts
@@ -2,14 +2,14 @@ import { ref, remove, get } from "firebase/database";
 import { db } from "@/lib/firebase/firebase";
 import { Game } from "@/lib/firebase/database";
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 export const GET = async (request: Request) => {
   console.log("Cleaning up lobbies...")
 
   const cutOffTime = Date.now() - 1000 * 60 * 5; // 5 minutes ago
   const gamesRef = ref(db, 'games');
-  const snapshot = await get(gamesRef)
+  const snapshot = await get(gamesRef);
   console.log("Data read successfully", snapshot.val());
 
   for (const [gameId, game] of Object.entries<Game>(snapshot.val())) {
