a4276c20d13f6b1d59940765bbdbe602fbaec63e
JesseCrans <jesse.crans@live.nl>
2024-02-15 20:31:17 +0100

    changed to then/catch
    Co-authored-by: Hugo van Schalm <HugovanSchalm@users.noreply.github.com>

--- src/app/api/lobby-cleanup/route.ts
+++ src/app/api/lobby-cleanup/route.ts
@@ -10,21 +10,24 @@ export const GET = async (request: Request) => {
 
   console.log("Cleaning up lobbies...", getID);
 
-  const cutOffTime = Date.now() - 1000 * 60 * 5; // 5 minutes ago
+  const cutOffTime = Date.now() - 1000 * 60 * 60 * 3; // 3 hours ago
   const gamesRef = ref(db, 'games');
 
   console.log("Getting games...", getID);
 
-  const snapshot = await get(gamesRef);
+  get(gamesRef).then((snapshot) => {
+    console.log("Data read successfully", getID, snapshot.val());
 
-  console.log("Data read successfully", getID, snapshot.val());
-
-  for (const [gameId, game] of Object.entries<Game>(snapshot.val())) {
-    if (game.lastMoveTime < cutOffTime) {
-      console.log(`Game ${gameId} is too old and will be removed.`, getID);
-      await remove(ref(db, `games/${gameId}`));
+    for (const [gameId, game] of Object.entries<Game>(snapshot.val())) {
+      if (game.lastMoveTime < cutOffTime) {
+        console.log(`Game ${gameId} is too old and will be removed.`, getID);
+        remove(ref(db, `games/${gameId}`)).catch((error) => {
+          console.error("Error deleting game", gameId, getID, error);
+        });
+      }
     }
-  }
-
+  }).catch((error) => {
+    console.error("Error reading data", getID, error);
+  });
   return new Response("Lobbies cleaned up.");
 }
