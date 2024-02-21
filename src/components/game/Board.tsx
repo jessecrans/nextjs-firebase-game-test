a796ebd375a063ee53fb3cba33499dfe87658907
JesseCrans <jesse.crans@live.nl>
2024-02-21 20:59:24 +0100

    Basic board
    Co-authored-by: Hugo van Schalm <HugovanSchalm@users.noreply.github.com>

--- src/components/game/Board.tsx
+++ src/components/game/Board.tsx
@@ -7,16 +7,23 @@ const Board = ({
   board: string[][]
 }) => {
   return (
-    <div className='grid grid-cols-3 grid-rows-3'>
+    <div className='grid grid-rows-3 grid-cols-3 gap-4 aspect-square max-w-xl m-auto min-w-72'>
       {
         board.map((row, rowIndex) => (
-          <div key={rowIndex} className='col-span-3 grid grid-cols-subgrid'>
-            {
-              row.map((cell, cellIndex) => (
-                <span key={cellIndex} className='text-center border-2'>{"X"}</span>
-              ))
-            }
-          </div>
+          row.map((cell, cellIndex) => (
+            <div key={cellIndex} className='bg-gray-400 bg-opacity-50 flex justify-center items-center relative'>
+              {
+                cell === 'X' ? (
+                  <>
+                    <div className="absolute inset-0 m-auto h-4 w-full bg-black rotate-45"></div>
+                    <div className="absolute inset-0 m-auto h-4 w-full bg-black -rotate-45"></div>
+                  </>
+                ) : (
+                  <div className='h-5/6 w-5/6 border-8 border-black rounded-full'></div>
+                )
+              }
+            </div>
+          ))
         ))
       }
     </div>
--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -9,6 +9,17 @@ const Game = ({
 }) => {
   return (
     <div>
+      <div className='m-4 mt-0'>
+        <p className="text-2xl">
+          {game.user1.name} (X)
+        </p>
+        <p>
+          vs
+        </p>
+        <p className="text-2xl">
+          {game.user2.name} (O)
+        </p>
+      </div>
       <Board board={game.board} />
     </div>
   )
